c3c66d08d8bd530832ce8032ef50a7732ecd749f
khoatran5773 <khoa.tran4@tiki.vn>
2021-03-21 20:59:33 +0700

    enhance export feature

--- test/testMongoConfig.js
+++ test/testMongoConfig.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-  host: 'uat-dandelion-mongodb.svr.tiki.services',
+  host: 'localhost',
   port: 27017,
-  dbName: 'dandelion_uat',
+  dbName: 'test',
   // eslint-disable-next-line max-len
-  makeConnectionUrl: () => 'mongodb://dandelionad:M0n90Bak82F0r3V3r@uat-dandelion-mongodb.svr.tiki.services:27017/dandelion_uat?connect=direct&authSource=admin',
+  makeConnectionUrl: () => 'mongodb://localhost:27017/test?connect=direct',
 };
